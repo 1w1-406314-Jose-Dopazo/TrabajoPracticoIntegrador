d4dee7fb9a3ea2c39884798b19559e6ed0c7beb3
1w1-406314-José-Dopazo <406314@tecnicatura.frc.utn.edu.ar>
2024-11-05 16:13:16 -0300

    controller funcional y cadena de conexion actualizada parcialmente

--- Api_Farmacia/Api_Farmacia/Program.cs
+++ Api_Farmacia/Api_Farmacia/Program.cs
@@ -1,5 +1,6 @@
 using Api_Farmacia.Data;
 using Api_Farmacia.Models;
+using Api_Farmacia.Services;
 using Microsoft.EntityFrameworkCore;
 using System.Data.SqlClient;
 
@@ -11,14 +12,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-var cnn = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<FarmaciaContext>(options => options.UseSqlServer(cnn));
+builder.Services.AddDbContext<FarmaciaContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<FarmaciaContext, FarmaciaContext>();
 builder.Services.AddScoped<ITipoUsuarioRepository, TipoUsuarioRepository>();
 builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 builder.Services.AddScoped<IDetalleFacturaRepository, DetalleFacturaRepository>();
 builder.Services.AddScoped<IFacturaRepository, FacturaRepository>();
 builder.Services.AddScoped<IMedicamentoRepository, MedicamentoRepository>();
+builder.Services.AddScoped<IFarmaciaService, FarmaciaService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
