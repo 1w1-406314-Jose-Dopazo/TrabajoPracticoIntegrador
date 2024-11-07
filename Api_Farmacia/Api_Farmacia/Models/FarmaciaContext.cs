f38eb247a84a04f2316aaaa676b7961b3ac0a11a
LauAtam <lautaroatampiz@gmail.com>
2024-11-07 13:52:37 -0300

    Borre el options del builder en farmaciaContext, corregi el builder en Program.cs y el nombre de la cadena de conexion en appsettings

--- Api_Farmacia/Api_Farmacia/Models/FarmaciaContext.cs
+++ Api_Farmacia/Api_Farmacia/Models/FarmaciaContext.cs
@@ -8,14 +8,7 @@ namespace Api_Farmacia.Models;
 
 public partial class FarmaciaContext : DbContext
 {
-    public FarmaciaContext()
-    {
-    }
-
-    public FarmaciaContext(DbContextOptions<FarmaciaContext> options)
-        : base(options)
-    {
-    }
+    public FarmaciaContext(DbContextOptions<FarmaciaContext> options) : base(options) { }
 
     public virtual DbSet<Cliente> Clientes { get; set; }
 
@@ -28,10 +21,6 @@ public partial class FarmaciaContext : DbContext
     public virtual DbSet<TipoUsuario> TiposUsuarios { get; set; }
 
     public virtual DbSet<Usuario> Usuarios { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-2LUFI20;Initial Catalog=Farmacia;Integrated Security=True;Trust Server Certificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- Api_Farmacia/Api_Farmacia/Program.cs
+++ Api_Farmacia/Api_Farmacia/Program.cs
@@ -8,19 +8,20 @@ using System.Data.SqlClient;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<FarmaciaContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<FarmaciaContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<FarmaciaContext, FarmaciaContext>();
 builder.Services.AddScoped<ITipoUsuarioRepository, TipoUsuarioRepository>();
 builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 builder.Services.AddScoped<IDetalleFacturaRepository, DetalleFacturaRepository>();
 builder.Services.AddScoped<IFacturaRepository, FacturaRepository>();
 builder.Services.AddScoped<IMedicamentoRepository, MedicamentoRepository>();
 builder.Services.AddScoped<IFarmaciaService, FarmaciaService>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
