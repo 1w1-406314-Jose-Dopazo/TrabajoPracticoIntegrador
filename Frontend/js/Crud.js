71eb8ed991320e9282ba3d4cd96f65f576da6335
LauAtam <lautaroatampiz@gmail.com>
2024-11-12 16:56:32 -0300

    Crud con Get y funciones para cargar tablas

--- Frontend/js/Crud.js
+++ Frontend/js/Crud.js
@@ -6,14 +6,47 @@ async function Get(url){
 }
 
 async function LoadTable(url){
-    const entities = Get(url)
+    const entities =  await Get(url)
     const $container = document.getElementById('container')
-    $container.appendChild(CreateTable(entities))
-    console.log(entities)
+    const table = await CreateTable(entities);
+    $container.appendChild(table)
 }
 
+async function CreateTable(entities) {
+  console.log(entities);
 
+  const table = document.createElement("table");
+  const thead = document.createElement("thead");
 
+  console.log(entities[0]);
+
+  const headers = Object.keys(entities[0]);
+  const titles = headers.map(e=>e.toUpperCase())
+  const headRow = document.createElement("tr");
+  headers.forEach((header) => {
+    const th = document.createElement("th");
+    th.textContent = header.toUpperCase();
+    headRow.appendChild(th);
+  });
+  thead.appendChild(headRow);
+  table.appendChild(thead);
+
+  const tbody = document.createElement("tbody");
+
+  const dataRows = Object.values(entities);
+
+  entities.forEach((entity) => {
+    const row = document.createElement("tr");
+    Object.values(entity).forEach((value) => {
+      const td = document.createElement("td");
+      td.textContent = value;
+      row.appendChild(td);
+    });
+    tbody.appendChild(row);
+  });
+  table.appendChild(tbody);
+  return table;
+}
 
 function New(url,nombre,descripcion,estado){
     fetch(url, {
@@ -25,34 +58,4 @@ function New(url,nombre,descripcion,estado){
         credentials: 'same-origin'
         
     })
-}
-
-function CreateTable(entities) {
-    const table = document.createElement("table");
-    const head = document.createElement("head");
-    const headers = Object.keys(entities[0]);
-    const headRow = document.createElement("tr");
-    headers.forEach(header => {
-        const th = document.createElement('th')
-        th.innerHTML(header);
-        headRow.appendChild(th);
-    })
-    head.appendChild(headRow);
-    table.appendChild(head);
-
-    const body = document.createElement("body")
-
-    const dataRows = Object.values(entities);
-
-    entities.forEach(entity => {
-        const row = document.createElement("tr")
-        Object.values(entity).forEach(value => {
-            const td = document.createElement("td");
-            td.innerHTML(value);
-            row.appendChild(td);
-        }
-        )
-        body.appendChild(row);
-    });
-    return table;
 }
