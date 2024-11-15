d39948b748c0f5ae1fcc783bd84b903b16b5b123
LauAtam <lautaroatampiz@gmail.com>
2024-11-15 06:05:52 -0300

    Unificacion de todos los scripts en main.js, dividido por regiones, vista de medicamentos completa, vista de facturas con nueva-modal y editar-modal a medias

--- Frontend/js/Entidades.js
+++ Frontend/js/Entidades.js
@@ -4,25 +4,129 @@ const localhost = "https://localhost:7263/api/";
 // UpdateMedicamento, DeleteMedicamento, CreateMedicamento
 
 //#region Medicamento
-function UpdateMedicamento(medicamento) {
-    
+async function UpdateMedicamento(medicamento) {
+        const response = await fetch('https://localhost:7263/api/Medicamento', {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(obj),
+        credentials: 'same-origin'
+        
+    })
+    if(response.ok){
+      alert('medicamento actualizado correctamente')
+    }
 }
 
 // O delete entidad, como lo veas mejor
-function DeleteMedicamento(id){
-
+async function DeleteMedicamento(id){
+    if (confirm("Eliminar medicamento?")) {
+        const response = await fetch(`https://localhost:7263/api/Medicamento/${id}`, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(obj),
+            credentials: 'same-origin'
+            
+        })
+    }
+    if(response.ok){
+      alert('medicamento Eliminado correctamente')
+    }
 }
 
-function CreateMedicamento(medicamento){
+async function CreateMedicamento(medicamento){
+    const response = await fetch('https://localhost:7263/api/Medicamento', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(obj),
+        credentials: 'same-origin'
+        
+    })
+    if(response.ok){
+      alert('medicamento Creado correctamente')
+    }
 
 }
 
 //#endregion Medicamento
 
 //#region Factura
+async function UpdateFactura(factura) {
+    const response = await fetch('https://localhost:7263/api/Factura', {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(obj),
+    credentials: 'same-origin'
+    
+})
+if(response.ok){
+  alert('factura actualizada correctamente')
+}
+}
+async function DeleteFactura(id){
+    const response = await fetch(`https://localhost:7263/api/Factura/${id}`, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(obj),
+    credentials: 'same-origin'
+    
+})
+if(response.ok){
+  alert('factura Eliminada correctamente')
+}
+}
 
+async function CreateFactura(factura){
+const response = await fetch('https://localhost:7263/api/Factura', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(obj),
+    credentials: 'same-origin'
+    
+})
+if(response.ok){
+  alert('factura Creada correctamente')
+}
+
+}
 //#endregion Factura
 
 //#region Clientes
+async function UpdateCliente(factura) {
+    const response = await fetch('https://localhost:7263/api/Cliente', {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(obj),
+    credentials: 'same-origin'
+    
+})
+if(response.ok){
+  alert('cliente actualizado correctamente')
+}
+}
+async function DeleteCliente(id){
+    const response = await fetch(`https://localhost:7263/api/Cliente/${id}`, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(obj),
+    credentials: 'same-origin'
+    
+})
+if(response.ok){
+  alert('cliente Eliminado correctamente')
+}
+}
 
+async function CreateCliente(factura){
+const response = await fetch('https://localhost:7263/api/Cliente', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(obj),
+    credentials: 'same-origin'
+    
+})
+if(response.ok){
+  alert('cliente Creado correctamente')
+}
+
+}
 //#endregion Clientes
--- Frontend/js/tablas.js
+++ Frontend/js/tablas.js
@@ -5,51 +5,70 @@ async function DeleteEntityById(url, id) {
   return await response.json();
 }
 
-// funcion que carga la tabla de medicamentos
 function LoadMedicamentos() {
   fetch(localhost + "/api/Medicamento")
     .then((response) => response.json())
-    .then((data) => {
+    .then((medicamentos) => {
       const tbody = document.getElementById("tbody-medicamentos");
       tbody.innerHTML = ""; // Limpiar la tabla
-      data.forEach((med) => {
-        const row = `
-                  <tr>
-                    <td>${med.nombre}</td>
-                    <td>${med.descripcion}</td>
-                    <td>${med.activo ? "Activo" : "Inactivo"}</td>
-                    <td>
-                      <div class="btn-group me-2">
-                        <button type="button" class="btn btn-outline-warning" data-bs-toggle="modal" data-bs-target="#editar-factura-modal">
-                          <i class="bi bi-pencil"></i>
-                        </button>
-                        <button onclick="deleteMedicamento(${med.id})" class="btn btn-outline-danger">
-                          <i class="bi bi-trash"></i>
-                        </button>
-                      </div>
-                    </td>
-                  </tr>
-              `;
-        tbody.innerHTML += row;
+
+      const modalEditarMedicamento = new bootstrap.Modal(document.getElementById('editar-medicamento-modal'))
+
+      medicamentos.forEach((medicamento) => {
+        const row = document.createElement("tr");
+        
+        row.innerHTML = `
+          <td>${medicamento.nombre}</td>
+          <td>${medicamento.descripcion}</td>
+          <td class="text-center">${medicamento.activo ? "Activo" : "Inactivo"}</td>
+          <td class="text-center">
+            <div class="btn-group me-2">
+              <button type="button" class="btn btn-outline-warning edit-btn" data-bs-toggle="modal" data-bs-target="#editar-factura-modal">
+                <i class="bi bi-pencil"></i>
+              </button>
+              <button onclick="deleteMedicamento(${medicamento.id})" class="btn btn-outline-danger delete-btn">
+                <i class="bi bi-trash"></i>
+              </button>
+            </div>
+          </td>
+        `;
+
+        // Agregar el evento para el botón de editar
+        row.querySelector(".edit-btn").addEventListener("click", function(){
+          modalEditarMedicamento.show()
+          console.log("Botón de editar presionado para:", medicamento);
+          // Mostrar modal
+        });
+        
+        // Agregar el evento para el botón de eliminar
+        row.querySelector(".delete-btn").addEventListener("click", function(){
+          DeleteMedicamento(medicamento.id);
+          console.log("Botón de eliminar presionado para:", medicamento);
+          // Eliminar medicamento
+        });
+
+        tbody.appendChild(row);
       });
     })
     .catch((error) => console.error("Error al cargar medicamentos:", error));
 }
 
+
 //Funcion que carga la tabla de facturas
 function LoadFacturas() {
   fetch(localhost + "/api/Factura")
     .then((response) => response.json())
-    .then((data) => {
+    .then((facturas) => {
       const tbody = document.getElementById("tbody-facturas");
       tbody.innerHTML = ""; // Limpiar la tabla
-      data.forEach((factura) => {
+      facturas.forEach((factura) => {
         const cliente = fetch(localhost + "api/Cliente/" + factura.idCliente).
         then((response) => response.json()).
         then((clienteJson) => clienteJson.nombre + " " + clienteJson.apellido)
         console.log(cliente)
         const row = `
                   <tr>
+                    <td class="d-none">${factura.id}</td>
                     <td>${cliente}</td>
                     <td>${factura.fecha}</td>
                     <td>
@@ -57,16 +76,29 @@ function LoadFacturas() {
                                     data-bs-target="#editar-factura-modal">
                         <i class="bi bi-pencil"></i>
                       </button>
-                      <button onclick="deleteMedicamento(${medicamento.id})" class="btn btn-outline-danger">
+                      <button onclick="deleteFactura(${factura.id})" class="btn btn-outline-danger">
                         <i class="bi bi-trash"></i>
                       </button>
-                      <button onclick="deleteMedicamento(${medicamento.id})" class="btn btn-outline-danger">
+                      <button onclick="infoFactura(${factura.id})" class="btn btn-outline-danger">
                         <i class="bi bi-trash"></i>
                       </button>
                     </td>
                   </tr>
               `;
-        tbody.innerHTML += row;
+        // Agregar el evento para el botón de editar
+        row.querySelector(".edit-btn").addEventListener("click", function(){
+          // Aquí puedes configurar la lógica del modal antes de abrirlo, si es necesario
+          console.log("Botón de editar presionado para:", medicamento);
+          // Mostrar modal
+        });
+        
+        // Agregar el evento para el botón de eliminar
+        row.querySelector(".delete-btn").addEventListener("click", function(){
+          // Aquí puedes configurar la lógica del modal antes de abrirlo, si es necesario
+          console.log("Botón de eliminar presionado para:", medicamento);
+          // Eliminar medicamento
+        });
+        tbody.appendChild(row);
       });
     })
     .catch((error) => console.error("Error al cargar medicamentos:", error));
